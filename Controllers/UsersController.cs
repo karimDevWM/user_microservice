f7e3d2d2e77ad0693c9209c4be93281f75f2f1b5
karimDevWM <k.trabelsi9@laposte.net>
2023-12-20 15:43:18 +0100

    initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using userMicroservice.Model;
+using userMicroservice.Services.Interface;
+using userMicroservice.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace userMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        // GET: api/<UsersController>private readonly IUserService userService;
+        public UsersController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+        [HttpGet]
+        public IEnumerable<User> UserList()
+        {
+            var userList = userService.GetUserList();
+            return userList;
+        }
+        [HttpGet("{id}")]
+        public User GetUserById(int id)
+        {
+            return userService.GetUserById(id);
+        }
+        [HttpPost]
+        public User AddUser(User user)
+        {
+            return userService.AddUser(user);
+        }
+        [HttpPut]
+        public User UpdateUser(User user)
+        {
+            return userService.UpdateUser(user);
+        }
+        [HttpDelete("{id}")]
+        public bool DeleteUser(int id)
+        {
+            return userService.DeleteUser(id);
+        }
+    }
+}
