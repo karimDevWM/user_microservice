498335f0bd63738815754ade61129a55de691f05
karimDevWM <k.trabelsi9@laposte.net>
2023-12-22 15:05:07 +0100

    add unit test, packages, and microservice structure

---
+++ IoC/IoCApplication/IoCApplication.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using userMicroservice.Data;
+
+namespace userMicroservice.IoC.IoCApplication
+{
+    public static class IoCApplication
+    {
+        public static IServiceCollection ConfigureDBContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DbContextClass>(options =>
+                options.UseSqlServer(connectionString));
+
+
+            return services;
+        }
+    }
+}
