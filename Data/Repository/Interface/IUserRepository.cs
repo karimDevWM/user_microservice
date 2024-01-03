3f62ccb304e957690f75f42e6c8613acbef07743
karimDevWM <k.trabelsi9@laposte.net>
2024-01-03 11:33:59 +0100

    update CD workflow

---
+++ Data/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using userMicroservice.Model;
+
+namespace userMicroservice.Data.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsersAsync();
+
+        Task<User> GetUserByIdAsync(int userId);
+
+        Task<User> CreateUserAsync(User user);
+
+        Task<User> UpdateUserAsync(User user);
+
+        Task<User> DeleteUserAsync(User user);
+    }
+}
